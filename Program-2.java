6c732876c9c5e35e85d296dfac76358578016071
yaminikoyyani <yaminikoyyani123@gmail.com>
2025-09-29 18:25:02 +0530

    Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddSeriesGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (a): ");
+        int a = sc.nextInt();
+
+        System.out.print("Output: ");
+        for (int i = 0; i < a; i++) {
+            System.out.print((2 * i + 1));
+            if (i < a - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        sc.close();
+    }
+}
